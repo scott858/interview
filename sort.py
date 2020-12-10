61a588cc4758e4bdde94726f8664150bae4996c6
Scott Lee <scott.lee@nucleusscientific.com>
2020-12-09 20:58:15 -0500

    heapsort

--- sort.py
+++ sort.py
@@ -74,28 +74,69 @@ def merge(left, right):
     return merged
 
 
-def insertion_sort(data):
-    # prett sure this is equivalent to bubble sort
-    data_out = []
-    for datum in data:
-        data_out.append(datum)
-        for i in range(len(data_out) - 1):
-            if datum > data_out[i]:
-                continue
-            else:
-                data_out[i], data_out[-1] = data_out[-1], data_out[i]
+def selection_sort(data):
+    data_len = len(data)
 
-    return data_out
+    for i in range(data_len):
+        small_ix = i
+        for j in range(i, data_len - 1):
+            next_ix = j + 1
+            if data[small_ix] > data[next_ix]:
+                small_ix = next_ix
+
+        data[small_ix], data[i] = data[i], data[small_ix]
+
+
+def insertion_sort(data):
+    data_len = len(data)
+    for i in range(1, data_len):
+        datum = data[i]
+        prev_ix = i - 1
+        prev_datum = data[prev_ix]
+        while datum < prev_datum and prev_ix >= 0:
+            print(data)
+            data[prev_ix + 1], data[prev_ix] = data[prev_ix], data[prev_ix + 1]
+            prev_ix -= 1
+            prev_datum = data[prev_ix]
+
+
+def heap_sort(data):
+    heapify(data)
+
+    for i in range(len(data) - 1, -1, -1):
+        data[i], data[0] = data[0], data[i]
+        heap_step(data, i, 0)
+
+
+def heap_step(data, n, root_ix):
+    left_child_ix = 2 * root_ix + 1
+    right_child_ix = 2 * root_ix + 2
+
+    if right_child_ix < n:
+        right_child = data[right_child_ix]
+
+        root = data[root_ix]
+
+        if right_child > root:
+            data[right_child_ix], data[root_ix] = data[root_ix], data[right_child_ix]
+            heap_step(data, n, right_child_ix)
+
+    if left_child_ix < n:
+        left_child = data[left_child_ix]
+
+        root = data[root_ix]
+
+        if left_child > root:
+            data[left_child_ix], data[root_ix] = data[root_ix], data[left_child_ix]
+            heap_step(data, n, left_child_ix)
+
+
+def heapify(data):
+    for i in range(len(data), -1, -1):
+        heap_step(data, len(data), i)
 
 
 if __name__ == '__main__':
-    data = [1, 50, 3, 12, 8]
-
-    print(data)
-
-    # bubble_sort(data)
-    # quick_sort(data, 0, len(data)-1)
-    # data = merge_sort(data)
-    data = insertion_sort(data)
-
+    data = [1, 50, 3, 12, 8, 34, 7, 29]
+    heap_sort(data)
     print(data)
